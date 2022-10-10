0f6ad5a35f67c1009748e1a24838d601f9ed4830
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-10-10 16:20:18 -0300

    feat: created routes and functions in controller for login

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LoginController::class, 'index'])->name('login');
+Route::post('/auth',[LoginController::class,'auth'])->name('auth.user');
+Route::post('/store',[LoginController::class,'store'])->name('store.user');
+Route::get('/register', [LoginController::class, 'register'])->name('register');
+Route::post('/logout',[LoginController::class,'logout'])->name('logout');
